87e080b0458a6916de80a352f561650f0c296113
saranya <saranyasambath007@gmail.com>
2021-06-19 13:13:04 +0530

    second commit

--- My program/src/Breakcontinue.java
+++ My program/src/Breakcontinue.java
@@ -4,10 +4,11 @@ public class Breakcontinue {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for (int i = 1; i <= 10; i++) {
+		for (int i = 1; i <= 10; i++) 
+		{
 			System.out.println(i);
 			if (i == 4) {
-				System.out.println("yes");
+				System.out.println("yes ");
 				continue;
 			}
 
